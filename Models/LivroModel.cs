d9c4b9e022025b671ea4167f36ec9ce02d45fd20
Luana Yasmim Lourenço <lylourenco@sercom.com.br>
2022-09-14 16:04:11 -0300

    sqlDetected

--- Models/LivroModel.cs
+++ Models/LivroModel.cs
@@ -5,36 +5,36 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API_Livros.Models
 {
-    [Table("Books")]
-    public class LivroModel : SupabaseModel
+    //[Table("Books")]
+    public class LivroModel //: SupabaseModel
     {
-        [PrimaryKey("id", false)]
+        //[PrimaryKey("id", false)]
         public int Id { get; set; }
 
-        [Column("book_name")]
+        //[Column("book_name")]
         //Utilizando o DataAnnotations para fazer validações
         [Required(ErrorMessage ="Informe o título!")]
         public string NomeLivro { get; set; }
 
-        [Column("author")]
+        //[Column("author")]
         [Required(ErrorMessage = "Informe o Autor(a)!")]
         public string NomeAutor { get; set; }
 
-        [Column("release_date")]
+        //[Column("release_date")]
         [Required(ErrorMessage = "Informe a data de lançamento!")]
         public DateTime DataLancamento { get; set; }
 
-        [Column("page_amount")]
+        //[Column("page_amount")]
         [Required(ErrorMessage = "Informe o número de páginas!")]
         public int NumPaginas { get; set; }
 
-        [Column("status")]
+        //[Column("status")]
         public bool StatusLivro { get; set; }
 
-        [Column("register_date")]
+        //[Column("register_date")]
         public DateTime DataCadastro { get; set; }
 
-        [Column("user_id")]
+        //[Column("user_id")]
         //Coluna que pega o id do usuário que cadastrar o livro
         public int? UserId { get; set; }
 
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -10,37 +10,37 @@ using System.Threading.Tasks;
 
 namespace API_Livros.Models
 {
-    [Table("Users")]
-    public class UserModel :SupabaseModel
+    //[Table("Users")]
+    public class UserModel //:SupabaseModel
     {
-        [PrimaryKey("user_id", false)]
+        //[PrimaryKey("user_id", false)]
         public int UserModelId { get; set; } // Chave primária
 
-        [Column("user_name")]
+        //[Column("user_name")]
         //[Required(ErrorMessage ="Informe o nome!")]
         public string NameUser { get; set; }
 
-        [Column("login")]
+        //[Column("login")]
         //[Required(ErrorMessage = "Informe o Usuário!")]
         public string LoginUser { get; set; }
 
-        [Column("user_email")]
+        //[Column("user_email")]
         //[Required(ErrorMessage = "Informe o Email!")]
         //[EmailAddress(ErrorMessage ="O email informado não é válido!")]
         public string EmailUser { get; set; }
 
-        [Column("user_profile")]
+        //[Column("user_profile")]
         //Required(ErrorMessage = "Informe o Perfil de Acesso!")]
         public ProfileEnum? ProfileUser { get; set; }
 
-        [Column("password")]
+        //[Column("password")]
         //[Required(ErrorMessage = "Informe a senha!")]
         public string PasswordUser { get; set; }
 
-        [Column("register_date")]
+        //[Column("register_date")]
         public DateTime RegisterDateUser { get; set; }
 
-        [Column("updade_date")]
+        //[Column("updade_date")]
         public DateTime? UpdateDateUser { get; set; } // ? - informa que o campo pode ser nulo
 
         //Busca a lista de livros 1 - n
